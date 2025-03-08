23a6d7e73806b0c8776c9be7f66e28b39463cd9b
Jashwanth <153377474+Jashwanth3@users.noreply.github.com>
2025-03-08 15:53:30 +0530

    Add files via upload

---
+++ StudentManagementApp/StudentLibrary/StudentLibrary/StudentRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public class StudentRepository//Data Accesss Class
+    {
+        private readonly string connString = "Server=WKSPUN05GTR1013;Database=STUDENTDB;Trusted_Connection=True";
+        public void AddStudent(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = $"INSERT INTO STUDENTS VALUES(@NAME,@AGE,@EMAIL)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NAME", student.Name);
+                cmd.Parameters.AddWithValue("@AGE", student.Age);
+                cmd.Parameters.AddWithValue("@EMAIL", student.Email);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public List<Student> GetAllStudents()
+        {
+            List<Student> students = new List<Student>();
+            using (SqlConnection conn = new SqlConnection(connString)) 
+            {
+                string query = "SELECT * FROM STUDENTS";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    students.Add(new Student
+                    {
+                        Id = Convert.ToInt32(reader["SID"]),
+                        Name = Convert.ToString(reader["SNAME"]),
+                        Age = Convert.ToInt32(reader["AGE"]),
+                        Email = Convert.ToString(reader["EMAIL"])
+                    });
+                }
+            }
+            return students;
+        }
+        public void UpdateStudents(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "UPDATE STUDENTS SET SNAME = @NAME, AGE = @AGE , EMAIL = @EMAIL WHERE SID = @ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", student.Id);
+                cmd.Parameters.AddWithValue("@NAME",student.Name);
+                cmd.Parameters.AddWithValue("@AGE", student.Age);
+                cmd.Parameters.AddWithValue("@EMAIL", student.Email);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void DeleteStudent(int Id)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "DELETE FROM STUDENTS WHERE SID = @ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", Id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
---
+++ StudentManagementApp/StudentManagementApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using StudentLibrary;
+
+namespace StudentManagementApp
+{
+    public class Program
+    {
+        static StudentRepository repo = new StudentRepository();
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("\n !!!!Student Management System!!!! ");
+                Console.WriteLine("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");   
+                Console.WriteLine("->1. Add Student\n->2. Update Student\n->3. Delete Student\n->4. View Students\n->5. Exit");
+                Console.WriteLine("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+                Console.WriteLine("Enter your choice: ");
+                int i = Convert.ToInt32(Console.ReadLine());
+                switch(i)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        UpdateStudent();
+                        break;
+                    case 3:
+                        DeleteStudent();                     
+                        break;
+                    case 4:
+                        ViewStudents();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("!!!!Enter Correct Value!!!!");
+                        break;
+                }
+            }
+        }
+        public static void AddStudent()
+        {
+            Console.WriteLine("Enter the following details of student: ");
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Email: ");
+            string email = Console.ReadLine();
+            repo.AddStudent(new Student { Name = name , Age = age , Email = email});
+            Console.WriteLine("+-+-+ Student Added Successfully +-+-+");
+        }
+        public static void ViewStudents()
+        {           
+            List<Student> students = repo.GetAllStudents();            
+            Console.WriteLine();
+            Console.WriteLine("Total Students: " + students.Count);
+            Console.WriteLine("Student Details: ");
+            Console.WriteLine("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            foreach (Student s in students)
+            {
+                Console.WriteLine("Id: "+ s.Id +"\t Name: " + s.Name + "\t Age: " + s.Age + "\t Email: " + s.Email);
+            }
+            Console.WriteLine("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            Console.WriteLine();
+        }
+        public static void UpdateStudent()
+        {
+            Console.WriteLine("Enter Student ID to update: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the new name: ");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Enter the new age: ");
+            int newAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the new email: ");
+            string newEmail = Console.ReadLine();
+            repo.UpdateStudents(new Student { Id = id, Name = newName , Age = newAge , Email = newEmail});
+            Console.WriteLine("+-+-+ Student Updated Successfully +-+-+");
+        }
+        public static void DeleteStudent()
+        {
+            Console.WriteLine("Enter the ID of student to delete: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            repo.DeleteStudent(id);
+            Console.WriteLine("+-+-+ Student Deleted Succesfully +-+-+");
+        }
+    }
+}
